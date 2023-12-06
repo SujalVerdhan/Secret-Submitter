acf92287cc2486318da3abacc9e93baf2d5b7283
Sujal Verdhan <sujalverdhen1234@gmail.com>
2023-12-06 10:39:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ userSchema.plugin(findOrCreate)
     passport.use(new GoogleStrategy({
         clientID:process.env.CLIENT_ID,
         clientSecret:process.env.CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/secrets",
+        callbackURL: "https://secretsubmitter.onrender.com/auth/google/secrets",
         userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
       },
       function(accessToken, refreshToken, profile, cb) {
